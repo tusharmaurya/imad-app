5da7bd216f79eef0596b49b1b312253f50bfb882
tusharmaurya <mmauryatushar@gmail.com>
2017-08-04 16:03:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send('Article one requested and will be served');
+});
+
+app.get('/article-two', function (req, res) {
+   res.send('Article two requested and will be served');
+});
+
+app.get('/article-three', function (req, res) {
+   res.send('Article three requested and will be served');
 });
 
 

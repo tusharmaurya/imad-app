8226c111b82b1cff65581c5fc7a14694bab315dd
tusharmaurya <mmauryatushar@gmail.com>
2017-08-04 17:19:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,47 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date= data.date;
+    var content = data.content;
+    var HTMLTemplate =  `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+             <link href = "/ui/style.css" rel="stylesheet"/>
+            <style>
+           
+            </style>
+        </head>
+       
+        <body>
+            <div class = "container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    
+    return(HTMLTemplate);
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +59,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
@@ -28,6 +69,24 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+
+var articleOne = {
+    title: 'Article One | Tushar Maurya',
+    heading: 'Article One',
+    date: 'Aug 4, 2017',
+    content: ` <p>
+                    content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here.
+                </p>
+                <p>
+                    content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here.
+                </p>
+                <p>
+                    content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here. content goes here.
+                </p>`
+    
+    
+};
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers

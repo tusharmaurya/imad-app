1f9754cfbb215735d9866cba047ca81445b16ef5
tusharmaurya <mmauryatushar@gmail.com>
2017-08-21 15:47:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
 var img = document.getElementById('madi');
+var marginLeft =0;
 function moveRight(){
-    marginLeft = marginLeft + 100;
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    
 }
 img.onclick = function () {
     var interval = setInterval(moveRight,100);

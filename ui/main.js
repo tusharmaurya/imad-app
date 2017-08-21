a91d145835b79fb77cf6982d8d7e7f78020894fc
tusharmaurya <mmauryatushar@gmail.com>
2017-08-21 15:45:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+var img = document.getElementById('madi');
+function moveRight(){
+    marginLeft = marginLeft + 100;
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight,100);
+};
